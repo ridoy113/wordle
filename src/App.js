380aca244d0801cc0d7e5f926f2f089f78fce961
ridoy113 <almamounhridoy113@gmail.com>
2022-07-16 12:15:44 +0600

    make components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
-import logo from './logo.svg';
 import './App.css';
+import Board from './components/Board';
+import Keyboard from './components/Keyboard';
 
 function App() {
   return (
     <div className='App'>
-
+      <nav>
+        <h1>Wordle</h1>
+      </nav>
+      <Board />
+      <Keyboard />
     </div>
   );
 }
---
+++ src/components/Board.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { useState } from 'react';
+import { boardDefault } from '../Words';
+
+const Board = () => {
+    const [board, setBoard] = useState(boardDefault)
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default Board;
